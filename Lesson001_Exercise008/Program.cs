111b311fe4f95bb60506a5e622d42345165d0527
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-06 19:36:44 +0300

    programm for Lesson1_Ex8 done_06/08/2022

--- Lesson001_Exercise008/Program.cs
+++ Lesson001_Exercise008/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive a number (N) and and show all even numbers from 1 to N
+
+Console.WriteLine("Enter your number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0) // Only positive numbers will be processed
+{
+int temp_n = 1;
+
+while (temp_n <= n)
+{
+    if (temp_n % 2 == 0)
+    {
+        Console.WriteLine(temp_n);
+        temp_n = temp_n + 1;
+    }
+    else
+    temp_n = temp_n + 1;
+}
+}
+else
+Console.WriteLine("Wrong number! Try again!");
