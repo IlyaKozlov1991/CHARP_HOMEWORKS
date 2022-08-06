463ac82ca7927817c1be251d9f732967fef7578a
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-06 18:44:57 +0300

    programm for Ex6 done_06/08/2022

--- Lesson001_Exercise006/Program.cs
+++ Lesson001_Exercise006/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive a number and check if it is even or odd
+
+Console.WriteLine("Enter your number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a % 2;
+
+if (b > 0)
+{
+    Console.WriteLine("No! Number is odd!");
+}
+else
+Console.WriteLine("Yes! Number is even!");
