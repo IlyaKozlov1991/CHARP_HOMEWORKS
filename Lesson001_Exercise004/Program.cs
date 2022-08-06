43eb799b6e5301efb1b287dcba3de5bba67ef4c2
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-06 18:21:37 +0300

    programm for Ex4 done_06/08/2022

--- Lesson001_Exercise004/Program.cs
+++ Lesson001_Exercise004/Program.cs
@@ -1,4 +1,25 @@
-﻿// Receive three numbers and find max
+﻿// Receive two numbers and find max
 
 Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Enter second number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+    Console.WriteLine("max = " + a);
+    }
+}
+else
+if (b > c)
+{
+Console.WriteLine("max = " + b);
+}
+else
+Console.WriteLine("max = " + c);
