438a2a6618cd7c4045f7bd71e86882e1b35237da
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-10 23:16:53 +0300

    Lesson002_Ex15 done_10/08/2022

--- Lesson002_Exercise015/Program.cs
+++ Lesson002_Exercise015/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive a number coresponding to a day and check if it is weekend day
+// First day of a week is MONDAY 
+
+Console.WriteLine("Enter your number: ");
+
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+if (UserNumber >= 8) // Check for correct entered number
+{
+    Console.WriteLine("Wrong number!");
+}
+else
+if (UserNumber == 6 | UserNumber == 7)
+{
+    Console.Write("Yes ");
+}
+else
+    Console.WriteLine("No ");
