5897141e5f42150495cb451bcb320cdf7bd7fbc2
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-14 22:54:10 +0300

    Lesson003_Ex021 done_14/08/2022

--- Lesson003_Exercise021/Program.cs
+++ Lesson003_Exercise021/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Find destance between two points in 3D space
+
+Console.WriteLine("Enter A(x): ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter A(y): ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter A(z): ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter B(x): ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("EnterB(y): ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("B(z): ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double Distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+Console.WriteLine("Distance AB found: " + Math.Round(Distance, 2));
