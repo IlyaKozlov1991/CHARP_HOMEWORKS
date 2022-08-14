3dd8c2f476a374813b5fc8db433b646d3bddc82c
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-14 23:36:18 +0300

    Lesson002_Ex023 done_14/08/2022

--- Lesson003_Exercise023/Program.cs
+++ Lesson003_Exercise023/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive a number (N) and show a table with with cubes from 1 to (N)
+
+Console.WriteLine("Enter your number");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (userNumber > 0) // Possible to operate with both positive and negative numbers
+{;
+    while (count <= userNumber)
+{
+    double cube = Math.Pow(count, 3);
+    count++;
+    Console.Write(cube + ", ");
+}
+return;
+}
+else
+count = -1;
+while (count >= userNumber)
+{
+    double cube = Math.Pow(count, 3);
+    count--;
+    Console.Write(cube + ", ");   
+}
