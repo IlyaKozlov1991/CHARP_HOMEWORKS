01d759ba62066de36502a55844b270d77da9889c
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-14 19:33:43 +0300

    Lesson003_Ex019 done_14/08/2022

--- Lesson003_Exercise019/Program.cs
+++ Lesson003_Exercise019/Program.cs
@@ -1,7 +1,21 @@
 ﻿// Receive 5-digits number and check if it is palindrome
 Console.WriteLine("Enter your number: ");
 
-string enterNumber = Console.ReadLine();
-Char[] inputData = enterNumber.ToCharArray();
-
-Console.WriteLine(inputData[2]);
+for (int Attempt = 0; Attempt < 5;) // User has ony 5 attempts
+{
+ string InputNumberStr = Console.ReadLine();
+ Char[] InputNumberChar = InputNumberStr.ToCharArray();   
+ if (InputNumberChar.Length == 5) // Check that 5-digits number has been entered
+{
+    if (InputNumberChar[0] == InputNumberChar[4] && InputNumberChar[1] == InputNumberChar[3])
+    {
+        Console.WriteLine("Yes, " + InputNumberStr + " is palindrome");
+    }
+    else Console.WriteLine("No, " + InputNumberStr + " is not palindrome");
+    return;
+}
+else
+Console.WriteLine("Wrong Number");
+Attempt++;
+}
+Console.WriteLine("Number must have FIVE digits");
