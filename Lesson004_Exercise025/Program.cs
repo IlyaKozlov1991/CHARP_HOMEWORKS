1c9241f7ac9ca37fbf96d671e200549ef22dde6b
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-18 20:51:12 +0300

    Lesson004_Ex025 done_18/08/2022

--- Lesson004_Exercise025/Program.cs
+++ Lesson004_Exercise025/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive two numbers (A) and (B) and exponent (A) into (B)  
+
+Console.WriteLine("Enter number A: ");
+int NumbA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number B: ");
+int NumbB = Convert.ToInt32(Console.ReadLine());
+
+double Result = Math.Pow(NumbA, Math.Abs(NumbB));
+
+Console.WriteLine($"Your result is: {Result}");
