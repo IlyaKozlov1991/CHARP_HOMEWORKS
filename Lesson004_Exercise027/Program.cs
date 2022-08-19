7cc1616de5cac0029f9c1cf76ba3d605f3cb3ea3
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-19 19:58:31 +0300

    still working on Ex027

--- Lesson004_Exercise027/Program.cs
+++ Lesson004_Exercise027/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive a number and show sum of all its numbers
+
+Console.WriteLine("Entre Your number");
+
+string NumbStr = Console.ReadLine();
+string[] TempStr = new string[NumbStr.Length];
+
+int[] NumbArr = new int[NumbStr.Length];
+int counter = 0;
+
+for (int i = 0; i < NumbStr.Length; i++)
+{
+    TempArr[i] = NumbStr.Substring(counter, 1);
+    counter++;
+}
+Console.WriteLine()
