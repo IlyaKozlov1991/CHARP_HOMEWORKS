244e68692225560ae4dace524af9186181a46784
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-20 12:07:43 +0300

    Lesson004_Ex029 done_20/08/2022

--- Lesson004_Exercise029/Program.cs
+++ Lesson004_Exercise029/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set an array with 8 elements and print it
+Console.WriteLine("Welcome to the Programming!!!");
+Console.WriteLine("Your Array is:");
+
+int[] MyArray = new int[8];
+
+for (int i = 0; i < MyArray.Length; i++)
+{
+    MyArray[i] = new Random().Next(-9, 9);
+    Console.Write($"{MyArray[i]}, ");
+}
