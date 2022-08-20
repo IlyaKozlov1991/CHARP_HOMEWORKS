4b84b03fb8d899db81170ecc810c5998ec66ad3e
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-08-20 14:39:32 +0300

    Lesson005_Ex036 done_20/08/2022

--- Lesson005_Exercise036/Program.cs
+++ Lesson005_Exercise036/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set an array filled with random numbers. Find sum of odd elements
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
+Console.WriteLine("Your Array is:");
+
+int[] MyArray = new int[5]; // Set new arrays with size of 5
+int Index = 0;
+int SumOdd = 0;
+
+while (Index < MyArray.Length)
+{
+    MyArray[Index] = new Random().Next(100, 500); // Random filling of the array with positive numbers from 100 to 500 
+    if (Index % 2 != 0) // Checking for odd numbers
+    {
+        SumOdd += MyArray[Index];
+    }
+    Console.Write($"{MyArray[Index]}, ");
+    Index++; 
+}
+Console.WriteLine("\n");
+Console.WriteLine($"Sum of odd elements is: {SumOdd}");
