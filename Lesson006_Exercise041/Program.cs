090903f69d899dbca5a81ac7c98869eef5e15544
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-08-24 11:00:32 +0300

    Lesson006_Ex041 done_24/08/2022

--- Lesson006_Exercise041/Program.cs
+++ Lesson006_Exercise041/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive (M) numbers from User and find how many of them are > 0
+
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
+Console.WriteLine("Enter Your Numbers using SAPCE");
+
+string NumberStr = Console.ReadLine();
+string[] SecondStr = NumberStr.Split(' ');
+ 
+int[] NumberArray = new int[SecondStr.Length];
+int Index = 0;
+int Count =0;
+
+Console.WriteLine("Your numbers: ");
+
+foreach (var i in SecondStr)
+{
+    NumberArray[Index] = Convert.ToInt32(i);
+    Console.Write($"{NumberArray[Index]} ");
+    if (NumberArray[Index] > 0)
+    {
+        Count++;
+        Index++;
+    }
+    else
+    {
+        Index++;
+    }
+}
+Console.WriteLine("\n");
+Console.WriteLine($"You entered {Count} numbers bigger than 0");
