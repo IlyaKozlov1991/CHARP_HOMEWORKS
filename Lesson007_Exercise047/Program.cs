de63c9f07fdf3627c89c2696886626e864ce16ee
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-08-28 17:19:53 +0300

    work on Lesson007_Ex047_28/08/2022

--- Lesson007_Exercise047/Program.cs
+++ Lesson007_Exercise047/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set-up 2-dimensional array [MxN] and fill it with random natural numbers
+
+Console.WriteLine("Enter M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] MyArray = new int[M, N];
+int IndexV = 0;
+int IndexH = 0;
+
+while (IndexH < MyArray.Length && IndexV < MyArray.Length)
+{
+for (int i = 0; i < M; i++)
+{
+    MyArray[i, 0] = new Random().Next(10, 10);
+    Console.WriteLine();
+    Console.WriteLine(MyArray[i, 0]);
+    Console.WriteLine();
+   
+    for (int j = 0; j < N; j++)
+{
+    MyArray[j, 0] = new Random().Next();
+    Console.WriteLine();
+    Console.WriteLine(MyArray[j, 0]);
+    Console.WriteLine();
+}
+}
+}
+IndexV++;
+IndexH++;
