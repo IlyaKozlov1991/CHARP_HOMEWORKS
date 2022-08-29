d7b49b16d5c0007628cc89a5aed9e1c1d515502a
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-08-29 10:52:30 +0300

    Lesson007 done_Ex050_29/08/2022

--- Lesson007_Exercise050/Program.cs
+++ Lesson007_Exercise050/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Receive indexes of an element and check if it is present in 2-dimensial array and its value
+
+Console.WriteLine("Parameters of Array are required");
+
+Console.WriteLine("Enter M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
+
+Console.WriteLine("Your Array:");
+
+int[,] MyArray = new int[M, N];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+        MyArray[i, j] = new Random().Next(-20, 20);
+        Console.Write($"{MyArray[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+
+Console.WriteLine("Enter Index I:");
+int IndexI = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Index J:");
+int IndexJ = Convert.ToInt32(Console.ReadLine());
+
+if (IndexI < M & IndexJ < N)
+{
+    Console.WriteLine(MyArray[IndexI, IndexJ]);
+}
+else
+{
+    Console.WriteLine("Such element doesn't exist in the Array");
+}
