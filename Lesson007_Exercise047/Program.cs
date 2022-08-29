88038e7dfde60ad589e5aa590620259921499d86
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-08-29 10:28:39 +0300

    Lesson007_Ex047 done_29/08/2022

--- Lesson007_Exercise047/Program.cs
+++ Lesson007_Exercise047/Program.cs
@@ -1,32 +1,26 @@
 ﻿// Set-up 2-dimensional array [MxN] and fill it with random natural numbers
 
+Console.WriteLine("Parameters of Array are required");
+
 Console.WriteLine("Enter M");
 int M = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter N");
 int N = Convert.ToInt32(Console.ReadLine());
 
-int[,] MyArray = new int[M, N];
-int IndexV = 0;
-int IndexH = 0;
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
 
-while (IndexH < MyArray.Length && IndexV < MyArray.Length)
-{
-for (int i = 0; i < M; i++)
-{
-    MyArray[i, 0] = new Random().Next(10, 10);
+Console.WriteLine("Your Array:");
+
+int[,] MyArray = new int[M, N];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+        MyArray[i, j] = new Random().Next(-100, 100);
+        Console.Write($"{MyArray[i, j]} ");
+        }
     Console.WriteLine();
-    Console.WriteLine(MyArray[i, 0]);
-    Console.WriteLine();
-   
-    for (int j = 0; j < N; j++)
-{
-    MyArray[j, 0] = new Random().Next();
-    Console.WriteLine();
-    Console.WriteLine(MyArray[j, 0]);
-    Console.WriteLine();
-}
-}
-}
-IndexV++;
-IndexH++;
+    }
