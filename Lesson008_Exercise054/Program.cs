2c2b986c9059b8c7d97f1b511cef6050a90428c5
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-09-04 10:08:16 +0300

    added folder for Lesson008_Ex058_04/09/2022

--- Lesson008_Exercise054/Program.cs
+++ Lesson008_Exercise054/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set-up a 2-dimensional array. Write a programm to put elements of each string in order: from bigger to lower 
+
+Console.WriteLine("Parameters of Array are required");
+
+Console.WriteLine("Enter number of strings");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
+
+Console.WriteLine("Your Array:");
+
+int[,] MyArray = new int[M, N];
+
+    for (int i = 0; i < MyArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < MyArray.GetLength(1); j++)
+        {
+        MyArray[i, j] = new Random().Next(1, 100);
+        Console.Write($" {MyArray[i, j]}  ");
+        }
+    Console.WriteLine();
+    }
+
+Console.WriteLine("Your array after formating: ");
+Console.WriteLine();
+
+for (int i = 0; i < MyArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < MyArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < MyArray.GetLength(1) - 1; k++)
+            {
+                if (MyArray[i, k] < MyArray[i, k + 1])
+                {
+                    int TempVar = MyArray[i, k + 1];
+                    MyArray[i, k + 1] = MyArray[i, k];
+                    MyArray[i, k] = TempVar;
+                }
+                Console.Write($"{MyArray[i, k]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+    return;
--- Lesson008_Exercise056/Program.cs
+++ Lesson008_Exercise056/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Parameters of Array are required");
+
+Console.WriteLine("Enter number of strings");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...");
+
+Console.WriteLine("Your Array: \n");
+
+int[,] MyArray = new int[M, N];
+
+    for (int i = 0; i < MyArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < MyArray.GetLength(1); j++)
+        {
+        MyArray[i, j] = new Random().Next(1, 100);
+        Console.Write($" {MyArray[i, j]}  ");
+        }
+    Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    int[] Sum = new int[MyArray.GetLength(1)];
+    for (int i = 0; i < MyArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < MyArray.GetLength(1); j++)
+        {
+            Sum[i] = Sum[i] + MyArray[i, j];
+            // Console.WriteLine($"i: {i}");
+            // Console.WriteLine($"j: {j}");
+        }
+        Console.WriteLine($"Sum of string {i}: {Sum[i]}");
+    }
+
+    int MinString = 0;
+    int FindMin = Sum[0]; 
+    for (int k = 1; k < Sum.GetLength(0) - 1; k++)
+        {
+            int TempVar = Sum[k];
+            if (TempVar < FindMin)
+            {
+                FindMin = TempVar;
+                MinString = k;
+            }
+        }
+    Console.WriteLine();
+    Console.WriteLine($"String #{MinString} has lowest Sum ofelements: {FindMin}");
+
