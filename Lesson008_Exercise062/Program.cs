9b396f09591619e16adbf0037bb1ea4f6fe4d95e
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-09-04 13:22:55 +0300

    Lesson008_Ex062 done_04/09/2022

--- Lesson008_Exercise062/Program.cs
+++ Lesson008_Exercise062/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set-up 2D array with size of 4x4 and fill it with numbers helix 
+
+Console.WriteLine("Start of the Programm");
+Console.WriteLine("...\n");
+
+int[,] MyArray = new int[4, 4];
+int TempVar = 1;
+int IndexI = 0;
+int IndexJ = 0;
+
+while (TempVar <= MyArray.GetLength(0) * MyArray.GetLength(1))
+{
+    MyArray[IndexI, IndexJ] = TempVar;
+    TempVar++;
+    if (IndexI <= IndexJ + 1 && IndexI + IndexJ < MyArray.GetLength(1) - 1)
+        IndexJ++;
+    else if (IndexI < IndexJ && IndexI + IndexJ >= MyArray.GetLength(0) - 1)
+        IndexI++;
+    else if (IndexI >= IndexJ && IndexI + IndexJ > MyArray.GetLength(1) - 1)
+        IndexJ--;
+    else
+        IndexI--;
+}
+Console.WriteLine();
+
+void WriteMyArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,-7}", array[i, j])); // For good look of output array
+        }
+        Console.WriteLine("\n");
+    }
+}
+
+WriteMyArray(MyArray);
+Console.WriteLine();
