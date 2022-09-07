ab6f525871219d97aea3514a088a4c5e1f1f9a80
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-09-07 18:02:33 +0300

    Lesson009_Ex064 done_07/09/2022

--- Lesson009_Exercise064/Program.cs
+++ Lesson009_Exercise064/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set the value to N. Write a program that prints all natural numbers between N and 1 using recursion.
+
+Console.WriteLine("\nEnter Your Number:\n");
+
+int N = int.Parse(Console.ReadLine());
+
+void RecursionN(int n)
+{
+    if (n == 1)
+    {
+        Console.Write("{0, -7}", n);
+    }
+    else
+    {
+        Console.Write("{0, -7}", n);
+        n--;
+        RecursionN(n);
+    }
+} 
+
+Console.WriteLine("\nResult N->1:\n");
+RecursionN(N);
+Console.WriteLine("\n");
