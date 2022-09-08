e13cc7d73362ae967549f5a93f5fdaabb5823f52
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-09-08 15:59:47 +0300

    added folder for Lesson009_Ex068_07/09/2022

--- Lesson009_Exercise066/Program.cs
+++ Lesson009_Exercise066/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Set the values of M and N. Write a program that will find the sum of positive integers between M and N using recursion
+
+Console.WriteLine("Enter Number M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Number N: ");
+int N = int.Parse(Console.ReadLine());
+
+void SumMN(int m, int n)
+{
+    if (m < n)
+    {
+        int i = 0;
+        i = i + m;
+        Console.Write($"{i}");
+        // i = i + m;
+        m++;
+        SumMN(m, n);
+    }
+    else
+    {
+        
+    }
+}
+
+SumMN(M, N);
